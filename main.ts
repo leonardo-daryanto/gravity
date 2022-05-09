d7743cfa394c691dead809e8642ba383db48901a
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-05-09 14:11:40 +0700

    Update pxt.json, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+//% weight=100 icon=ABC color=#000000
+namespace text {
+    /**
+     *  textsring. e.g.: text: Battle it now!
+     */
+    //% block
+    export function text (text: string): void {
+        let textSprite = textsprite.create(text)
+    }
+}
